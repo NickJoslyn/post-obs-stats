7a09bc152ab8a7426e46e8eec64a771d01b7d11b
Nick <nickj.joslyn@gmail.com>
2018-07-16 16:06:49 -0700

    First test of NETBUFST

--- packet_information.py
+++ packet_information.py
@@ -13,3 +13,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
+
+## Locations == /mnt_blc00/datax/dibas/AGBT18A_999_77/GUPPI/BLP00
+
+numberOfScans = int(subprocess.check_output("ls /mnt_blc00/datax/dibas/AGBT18A_999_77/GUPPI/BLP00/*gpuspec..headers | wc -l",shell=True)[:-1])
+numberOfBanks = 3
+numberOfNodes = 8
+NETBUFST_waterfall = np.zeros((numberOfBanks*numberOfNodes, numberOfScans))
+
+################################################################################
+### NETBUFST
+
+for bank in numberOfBanks:
+    for node in numberOfNodes:
+        NETBUFST_command = """for i in /mnt_blc""" + str(bank) + str(node) + """/datax/dibas/AGBT18A_999_77/GUPPI/BLP""" + str(bank) + str(node) + """/*gpuspec..headers; do /usr/bin/fold -w80 $i | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}' | awk '{ total += $1} END {print total/NR}'; done"""
+        NETBUFST_waterfall[(bank*numberOfNodes + node),:] = subprocess.check_output(NETBUFST_command, shell=True)[:-1].split("\n")
+
+plt.imshow(NETBUFST_waterfall)
+plt.colorbar()
+plt.clim(0,24)
+plt.show()
+################################################################################
+### NDROP
+
+
+
+
+
+
+################################################################################
+### PKTIDX
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Gets the NETBUFST value
+#/usr/bin/fold -w80 blc00_guppi_58278_52387_HIP1125_0057.gpuspec..headers | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}'
+
+# Gets the average NETBUFST value
+#/usr/bin/fold -w80 blc00_guppi_58278_52387_HIP1125_0057.gpuspec..headers | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}' | awk '{ total += $1} END {print total/NR}'
+
+# blc00:
+# test = """/usr/bin/fold -w80 /mnt_blc00/datax/dibas/AGBT18A_999_77/GUPPI/BLP00/blc00_guppi_58278_52387_HIP1125_0057.gpuspec..headers | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}' | awk '{ total += $1} END {print total/NR}'"""
+# float(subprocess.check_output(test, shell = True)[:-1])
+
+### full : for i in /mnt_blc00/datax/dibas/AGBT18A_999_77/GUPPI/BLP00/*gpuspec..headers; do /usr/bin/fold -w80 $i | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}' | awk '{ total += $1} END {print total/NR}'; done
