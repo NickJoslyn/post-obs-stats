179b1ff64dc9ebf483a41d9a00ae44a6bc2b36a2
Nick <nickj.joslyn@gmail.com>
2018-07-19 12:56:29 -0700

    Transpose axes

--- packet_information.py
+++ packet_information.py
@@ -32,7 +32,7 @@ cmap = mcolors.LinearSegmentedColormap(
 numberOfScans = int(subprocess.check_output("ls /mnt_blc00/datax/dibas/AGBT18A_999_77/GUPPI/BLP00/*gpuspec..headers | wc -l",shell=True)[:-1])
 numberOfBanks = 3
 numberOfNodes = 8
-NETBUFST_waterfall = np.zeros((numberOfScans, numberOfBanks*numberOfNodes))
+NETBUFST_waterfall = np.zeros((numberOfBanks*numberOfNodes, numberOfScans))
 
 ################################################################################
 ### NETBUFST
@@ -40,7 +40,7 @@ computeNodeNames = []
 for bank in range(numberOfBanks):
     for node in range(numberOfNodes):
         NETBUFST_command = """for i in /mnt_blc""" + str(bank) + str(node) + """/datax/dibas/AGBT18A_999_77/GUPPI/BLP""" + str(bank) + str(node) + """/*gpuspec..headers; do /usr/bin/fold -w80 $i | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}' | awk 'BEGIN {max = 0} {if ($1 > max) max = $1} END {print max}'; done"""
-        NETBUFST_waterfall[:,(bank*numberOfNodes + node)] = subprocess.check_output(NETBUFST_command, shell=True)[:-1].split("\n")
+        NETBUFST_waterfall[(bank*numberOfNodes + node), :] = subprocess.check_output(NETBUFST_command, shell=True)[:-1].split("\n")
         computeNodeNames.append('blc' + str(bank) + str(node))
 
 scanName_command = """ls /mnt_blc00/datax/dibas/AGBT18A_999_77/GUPPI/BLP00/*.gpuspec..headers | awk '{print substr($1, 75, index($1,".")-75)}'"""
@@ -52,8 +52,8 @@ plt.colorbar()
 plt.clim(0,24)
 #plt.ylabel("Source")
 #plt.xlabel("Compute Node")
-plt.xticks(np.arange(numberOfBanks*numberOfNodes), computeNodeNames, rotation=90)
-plt.yticks(np.arange(numberOfScans), scanNames)
+plt.yticks(np.arange(numberOfBanks*numberOfNodes), computeNodeNames)
+plt.xticks(np.arange(numberOfScans), scanNames, rotation = 90)
 plt.show()
 ################################################################################
 ### NDROP
