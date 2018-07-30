d804181ef278153f0d137cf3bc2f473a82f598e3
Nick <nickj.joslyn@gmail.com>
2018-07-30 12:41:24 -0700

    Twin y axes, prints

--- packet_information.py
+++ packet_information.py
@@ -16,9 +16,18 @@ cdict = {'red':   ((0.0, 0.0, 0.0),
          'green': ((0.0, 0.0, 1.0),
                    (0.5, 0.0, 0.0),
                    (1.0, 0.0, 0.0))}
+cdict2 = {'red':   ((0.0, 0.0, 0.0),
+                   (0.5, 0.0, 0.0),
+                   (1.0, 1.0, 1.0)),
+         'green': ((0.0, 0.0, 1.0),
+                   (0.5, 0.0, 0.0),
+                   (1.0, 0.0, 0.0))}
 
 cmap = mcolors.LinearSegmentedColormap(
 'my_colormap', cdict, 100)
+
+cmap2 = mcolors.LinearSegmentedColormap(
+'my_colormap', cdict2, 100)
 
 numberOfNodes = 8
 TOTAL_COMPUTE_NODES = ['00', '01', '02', '03', '04', '05', '06', '07', '10', '11', '12', '13', '14', '15', '16', '17', '18', '20', '21', '22', '23', '24', '25', '26', '27', '30', '31', '32', '33', '34', '35', '36', '37']
@@ -42,6 +51,7 @@ NETBUFST_waterfall = np.zeros((numberOfBanks*numberOfNodes, numberOfScans))
 computeNodeNames = []
 
 for bank in range(numberOfBanks):
+    print("Analyzing blc" + str(ACTIVE_COMPUTE_NODES[bank,0][0])+"*")
     for node in range(numberOfNodes):
         NETBUFST_command = """for i in /mnt_blc""" + str(ACTIVE_COMPUTE_NODES[bank,node]) + """/datax/dibas/""" + SESSION_IDENTIFIER + """/GUPPI/BLP""" + str(bank) + str(node) + """/*gpuspec..headers; do /usr/bin/fold -w80 $i | grep NETBUFST | awk '{print substr($2,2, index($2,"/")-2)}' | awk 'BEGIN {max = 0} {if ($1 > max) max = $1} END {print max}'; done"""
         try:
@@ -62,12 +72,14 @@ plt.clim(0,24)
 #plt.xlabel("Compute Node")
 plt.yticks(np.arange(numberOfBanks*numberOfNodes), computeNodeNames)
 plt.xticks(np.arange(numberOfScans), scanNames, rotation = 90)
+plt.tick_params(labelright = True)
 plt.tight_layout()
 plt.show()
 ################################################################################
 ### NDROP
 NDROP_waterfall = np.zeros((numberOfBanks*numberOfNodes, numberOfScans))
 for bank in range(numberOfBanks):
+    print("Analyzing blc" + str(ACTIVE_COMPUTE_NODES[bank,0][0])+"*")
     for node in range(numberOfNodes):
         NDROP_command = """for i in /mnt_blc""" + str(ACTIVE_COMPUTE_NODES[bank,node]) + """/datax/dibas/""" + SESSION_IDENTIFIER + """/GUPPI/BLP""" + str(bank) + str(node) + """/*gpuspec..headers; do /usr/bin/fold -w80 $i | grep NDROP | awk '{print $3}' | sort | uniq -c | awk '{print $1 * $2}' | awk '{total += $1} END {print 100*(total/(NR*16384))}'; done"""
         try:
@@ -84,6 +96,7 @@ plt.clim(0,100)
 #plt.xlabel("Compute Node")
 plt.yticks(np.arange(numberOfBanks*numberOfNodes), computeNodeNames)
 plt.xticks(np.arange(numberOfScans), scanNames, rotation = 90)
+plt.tick_params(labelright = True)
 plt.tight_layout()
 plt.show()
 
@@ -93,6 +106,7 @@ plt.show()
 ### PKTIDX
 PKTIDX_waterfall = np.zeros((numberOfBanks*numberOfNodes, numberOfScans))
 for bank in range(numberOfBanks):
+    print("Analyzing blc" + str(ACTIVE_COMPUTE_NODES[bank,0][0])+"*")
     for node in range(numberOfNodes):
         PKTIDX_command = """for i in /mnt_blc""" + str(ACTIVE_COMPUTE_NODES[bank,node]) + """/datax/dibas/""" + SESSION_IDENTIFIER + """/GUPPI/BLP""" + str(bank) + str(node) + """/*gpuspec..headers; do /usr/bin/fold -w80 $i | grep PKTIDX | awk '{print $3 - p; p = $3}' | sort | uniq -c | awk 'BEGIN{sum=0; number = 0}{number += $1}{if ($2>16384) sum += $1 * ($2/16384 - 1)} END {print sum/number*100}'; done"""
         try:
@@ -102,12 +116,13 @@ for bank in range(numberOfBanks):
             print("PKTIDX Problem with " + str(ACTIVE_COMPUTE_NODES[bank,node]))
 
 plt.title("Percentage of Blocks Dropped: " + SESSION_IDENTIFIER)
-plt.imshow(PKTIDX_waterfall, cmap = cmap)
+plt.imshow(PKTIDX_waterfall, cmap = cmap2)
 plt.colorbar()
 plt.clim(0,100)
 #plt.ylabel("Source")
 #plt.xlabel("Compute Node")
 plt.yticks(np.arange(numberOfBanks*numberOfNodes), computeNodeNames)
 plt.xticks(np.arange(numberOfScans), scanNames, rotation = 90)
+plt.tick_params(labelright = True)
 plt.tight_layout()
 plt.show()
